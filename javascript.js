de8b5819ad2b5c9b9222164f0f606238370042ab
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-09-28 20:15:04 -0400

    added index javascript and pseudo code

---
+++ javascript.js
@@ -0,0 +1,24 @@
+//variable declaritions
+    //timeRemaining
+//querySelectors
+
+//function to start the game
+//listen for button start
+//start timer in intervals of 1000 ms
+//first question displays and corresponding answers
+    //carousel-box like array to keep track of what question you are on
+//use eventlisteners on buttons check for answers
+    //compare clicked button to what we have as correct answer 
+    //if wrong display WRONG!!! and decrement an amount of time from timer
+    //if correct display CORRECT!!! 
+    //in either case move onto next question
+    //if end of questions end game
+//repeat until timer hits 0 or run out of questions
+    //when our array of questions returns undefined or length-1 for index
+    //end game
+//Display of time remaining as score
+//end game
+    //stop timer
+    //prompt for their initials 
+    //save score into high score
+    //clear timer
