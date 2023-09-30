57ae663e103314a9f121a5d2c9698cea5f2fba31
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-09-30 16:29:07 -0400

    add function to display question and possible answers

--- javascript.js
+++ javascript.js
@@ -8,31 +8,51 @@ var btn = document.createElement("button");
 var main = document.querySelector("main");
 var timeRemaining;
 var timer;
+var currentQuestion = 0;
 var questions = {
     q: ["Which of the following is NOT a semantic element?",
         "Which of the following is NOT a class selector?",
         "Which of the following is necessary to link a Javascript file with your html file?",
         "Which of the following sets <p> to say 'Hello'",
         "What was the answer to question 1?",
-        "The correct answer is a"],
+        "The correct answer is 'a'"],
     a: [["main", "header", "div", "section"],
         [".box", "#light", ".fans", ".dark"],
         ["src", "href", "ref", "a"],
         ["p.textContent('Hello')", "p.setAttribute('Hello')", "p = texContent('Hello')", "p.textContent = 'Hello'"],
         ["#light", "div", "src", "main"],
         ["a", "b", "c","d"]],
-    correct: [2, 1, 0, 3, 1]
+    correct: [2, 1, 0, 3, 1, 0]
 }
+answers.appendChild(ans1);
+answers.appendChild(ans2);
+answers.appendChild(ans3);
+answers.appendChild(ans4);
 main.appendChild(btn);
 btn.textContent = "Start Quiz";
 
-
 function startGame() {
     timeRemaining = 50;
+    currentQuestion = 0;
+    showQuestion();
 }
 
 function showQuestion() {
+    if(currentQuestion === questions.q.length) {
+        endGame();
+        return;
+    }
+    question.textContent = questions.q[currentQuestion];
+    main.removeChild(btn);
+    question.appendChild(answers);
+    ans1.textContent = questions.a[currentQuestion][0];
+    ans2.textContent = questions.a[currentQuestion][1];
+    ans3.textContent = questions.a[currentQuestion][2];
+    ans4.textContent = questions.a[currentQuestion][3];
+}
 
+function updateTimer() {
+    
 }
 
 btn.addEventListener("click", startGame);
