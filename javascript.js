0f5273f967860318defb0a2337a51d90dae07b74
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-01 13:25:57 -0400

    add comments to javascript for clarity

--- javascript.js
+++ javascript.js
@@ -2,16 +2,17 @@ var question = document.querySelector("h1");
 var answers = document.createElement("ol");
 var ans1 = document.createElement("li");
 var ans2 = document.createElement("li");
-var ans3 = document.createElement("li");
-var ans4 = document.createElement("li");
+var ans3 = document.createElement("li");    //declaration of elements that are being
+var ans4 = document.createElement("li");    //created and manipulated
 var btn = document.createElement("button");
 var main = document.querySelector("main");
 var header = document.querySelector("header");
 var highScoreDisplay = document.querySelector("#highScore");
-var timeRemaining;
+var highScoreEntry = JSON.parse(localStorage.getItem("hiScore"));
 var timerText = document.querySelector("#timer");
-var currentQuestion = 0;
-var questions = {
+var currentQuestion;
+var timeRemaining;
+var questions = { //object with each question, the answer, and which answer is correct in the array
     q: ["Which of the following is NOT a semantic element?",
         "Which of the following is NOT a class selector?",
         "Which of the following is necessary to link a Javascript file with your html file?",
@@ -26,7 +27,9 @@ var questions = {
         ["a", "b", "c","d"]],
     correct: [2, 1, 0, 3, 1, 0]
 }
-var highScoreEntry = JSON.parse(localStorage.getItem("hiScore"));
+
+//checks to see there were scores already saved in local storage
+//if not, set the entry to empty arrays
 if(highScoreEntry === null) {
     highScoreEntry = {
         name: [],
@@ -34,13 +37,17 @@ if(highScoreEntry === null) {
     }
 }
 
+//assigns our listed items to our ordered list that will display our answers
 answers.appendChild(ans1);
 answers.appendChild(ans2);
 answers.appendChild(ans3);
 answers.appendChild(ans4);
-main.appendChild(btn);
+main.appendChild(btn); //adds our created button to the main element
 btn.textContent = "Start Quiz";
 
+//sets the initial state once the user wants to start the game
+//sets our timer, current question
+//also removes visuals that aren't necessary during quiz
 function startGame() {
     timeRemaining = 50;
     currentQuestion = 0;
@@ -59,10 +66,12 @@ function startGame() {
     },1000)
 }
 
+
+//updates what question and answers are currently being shown
 function showQuestion() {
-    console.log("currentQuestion: " + currentQuestion);
-    console.log("# of questions: " +questions.q.length);
-    if(currentQuestion === questions.q.length) {
+    //checks if final question has been shown
+    //ends the game if true
+    if(currentQuestion === questions.q.length) { 
         endGame();
         return;
     }
@@ -76,10 +85,13 @@ function showQuestion() {
     }
 }
 
+//updates the timer
 function updateTimer() {
     timerText.textContent = "Timer: " + timeRemaining;
 }
 
+//prompts user for intials input then saves initials and score to localStorage
+//also sets quiz ready to be taken again if desired
 function endGame(){
     var initials = prompt("Please enter your initials!");
     clearInterval(timer);
@@ -92,6 +104,8 @@ function endGame(){
     header.appendChild(highScoreDisplay);
 }
 
+//checks if the user selected the correct answer
+//if user answered wrong, we take away 5 seconds from timer and update
 function checkAnswer(event){
     if(event.target.textContent === questions.a[currentQuestion][questions.correct[currentQuestion]]){
     }
@@ -103,14 +117,15 @@ function checkAnswer(event){
     showQuestion();
 }
 
-function swapScore(){
+//swaps whether a high scores list is shown
+function swapScore(){ //only adds new elements if there are new scores
     if(highScore.getAttribute("data-state")==="off" && 
         highScoreEntry.name.length > highScoreDisplay.childElementCount){
         highScore.setAttribute("data-state", "on");
         highScore.setAttribute("class","on");
         for(var x = 0; x < highScoreEntry.name.length; x++){
             tempLi = document.createElement("li");
-            if(highScoreEntry.name[x]===null){
+            if(highScoreEntry.name[x]===null){ //if user didn't enter intials set name = NA
                 tempLi.textContent = "NA: " + highScoreEntry.score[x];
             }
             else{
@@ -129,6 +144,8 @@ function swapScore(){
         }
     }
 }
+
+//different event "click" listeners
 highScore.addEventListener("click", swapScore);
 ans1.addEventListener("click",checkAnswer);
 ans2.addEventListener("click",checkAnswer);
