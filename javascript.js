03ee6f0ffb88592ba7970777df39c14cfeffe83d
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-01 03:51:46 -0400

    removed pseudocode from js file

--- javascript.js
+++ javascript.js
@@ -38,7 +38,6 @@ answers.appendChild(ans1);
 answers.appendChild(ans2);
 answers.appendChild(ans3);
 answers.appendChild(ans4);
-
 main.appendChild(btn);
 btn.textContent = "Start Quiz";
 
@@ -87,7 +86,7 @@ function endGame(){
     highScoreEntry.name.push(initials);
     highScoreEntry.score.push(timeRemaining);
     question.removeChild(answers);
-    question.textContent = "Do you want to play Again?";
+    question.textContent = "Want to try again?";
     main.appendChild(btn);
     localStorage.setItem("hiScore", JSON.stringify(highScoreEntry));
     header.appendChild(highScoreDisplay);
@@ -105,7 +104,6 @@ function checkAnswer(event){
 }
 
 function swapScore(){
-
     if(highScore.getAttribute("data-state")==="off" && 
         highScoreEntry.name.length > highScoreDisplay.childElementCount){
         highScore.setAttribute("data-state", "on");
@@ -131,35 +129,9 @@ function swapScore(){
         }
     }
 }
-
-
 highScore.addEventListener("click", swapScore);
 ans1.addEventListener("click",checkAnswer);
 ans2.addEventListener("click",checkAnswer);
 ans3.addEventListener("click",checkAnswer);
 ans4.addEventListener("click",checkAnswer);
 btn.addEventListener("click",startGame);
-//variable declaritions
-    //timeRemaining
-//querySelectors
-
-//function to start the game
-//listen for button start
-//start timer in intervals of 1000 ms
-//first question displays and corresponding answers
-    //carousel-box like array to keep track of what question you are on
-//use eventlisteners on buttons check for answers
-    //compare clicked button to what we have as correct answer 
-    //if wrong display WRONG!!! and decrement an amount of time from timer
-    //if correct display CORRECT!!! 
-    //in either case move onto next question
-    //if end of questions end game
-//repeat until timer hits 0 or run out of questions
-    //when our array of questions returns undefined or length-1 for index
-    //end game
-//Display of time remaining as score
-//end game
-    //stop timer
-    //prompt for their initials 
-    //save score into high score
-    //clear timer
